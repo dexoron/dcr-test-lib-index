7db539678230505ad99226b3b27cf46a95b12e68
Dexoron <probrovova1@gmail.com>
2026-05-12 20:22:26 +0800

    first commit

---
+++ src/dcr-math.c
@@ -0,0 +1,6 @@
+#include "dcr-math.h"
+
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
+float div_nums(int a, int b) { return (float)a / b; }
---
+++ src/dcr-math.h
@@ -0,0 +1,9 @@
+#ifndef DCR_MATH_H
+#define DCR_MATH_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+float div_nums(int a, int b);
+
+#endif
